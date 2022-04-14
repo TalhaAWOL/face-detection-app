6b51511b8c7ec56dad3daef10c1bbc5c76e297b0
TalhaAWOL <mtalhaibne@gmail.com>
2022-04-14 18:10:33 -0400

    detect form skeleten added

---
+++ src/components/DetectForm/DetectForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './DetectForm.css';
+
+const DetectForm = () => {
+    return(
+        <div className="detectForm">
+            <input type="text" />
+            <button>Detect</button>
+        </div>
+    );
+};
+
+export default DetectForm;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from '../../components/Navbar/Navbar'
+import DetectForm from '../../components/DetectForm/DetectForm'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <DetectForm /> */}
+      <DetectForm />
     </div>
   );
 }
