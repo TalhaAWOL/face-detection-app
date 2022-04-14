c754ee4fff5874e8048fa9f1e5c67ef3fa13ba3f
TalhaAWOL <mtalhaibne@gmail.com>
2022-04-14 16:23:49 -0400

    logo added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Logo.css';
+import logo from './Logo1.png'
 
 const Logo = () => {
     return(
-        <div>
-            <img src="https://www.flaticon.com/free-icon/face-detection_2706914?term=face&page=1&position=3&page=1&position=3&related_id=2706914&origin=search" alt="Logo" />
+        <div className="logoDiv">
+            <img src={logo} alt="Logo" />
         </div>
     );
 };
