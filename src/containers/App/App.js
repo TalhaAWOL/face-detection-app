a4fc68286a131bdddfabdc63995f1c9c70d5d2c6
TalhaM <mtalhaibne@gmail.com>
2022-04-15 16:45:54 -0400

    Front end with particles added

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,18 +2,32 @@ import React, {Component} from 'react';
 import './App.css';
 import Navbar from '../../components/Navbar/Navbar'
 import DetectForm from '../../components/DetectForm/DetectForm'
+import Particles from 'reactparticles.js'
+import config from './particlesjs-config.json'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <p className="f2 white">You are rank #5</p>
-        <p className="f4 white">The application will detect the faces on any image. Paste an image below to try.</p>
-        <DetectForm />
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Particles
+            id="config-1"
+            config="./particlesjs-config.json"
+            style={{
+              width: "100%",
+              height: "90vh",
+              opacity: "1"
+            }}
+            className="particles-class-name zNeg"
+          />
+          <p className="f2 white">You are rank #5</p>
+          <p className="f4 white">The application will detect the faces on any image. Paste an image below to try.</p>
+          <DetectForm />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
